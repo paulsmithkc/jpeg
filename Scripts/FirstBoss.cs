2e3a452ef99b4924e286fe5fd7f303fd3fca143d
paulsmithkc <paulsmithkc@gmail.com>
2015-01-24 22:53:52 -0600

    Adding sound to first boss

--- Scripts/FirstBoss.cs
+++ Scripts/FirstBoss.cs
@@ -4,17 +4,24 @@ using System.Collections;
 public class FirstBoss : MonoBehaviour {
 	private GameObject player;
 	private Animator animator;
+	private AudioSource audioSource;
+
+
+	public AudioClip squishSound;
 
 	void Start()
 	{
 		player = GameObject.Find ("Player");
 		animator = GetComponent<Animator>();
+		audioSource = gameObject.AddComponent<AudioSource>();
 	}
 
 	void Die()
 	{
 		Destroy(player.GetComponent<DamageOnCollide>());
 		animator.SetTrigger("Squish");
+		audioSource.PlayOneShot(squishSound);
+
 		//Destroy(gameObject);
 	}
 
